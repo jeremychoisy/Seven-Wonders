7f2dc0cce29e9c4c53b01ab37bd7cd68e58d7eab
Kloril <maxime433.prevot@gmail.com>
2019-02-12 20:43:12 +0100

    ajout methodes classe Carte

--- Model/src/main/java/org/Model/Carte.java
+++ Model/src/main/java/org/Model/Carte.java
@@ -2,13 +2,40 @@ package org.Model;
 
 public class Carte {
 	private String nom;
-	private String couleur;
+	private String type;
+	private int cout;
+	private int configurationNumber;
+	private int age;
+	private boolean pose;
 
 	public Carte() {}
 	
-	public Carte(String nom, String couleur){
+	public Carte(String nom, String type, int cout, int age, boolean pose){
 		this.nom = nom;
-		this.couleur = couleur;
+		this.type = type;
+		this.cout=cout;
+		this.age=age;
+		this.pose=pose;
+	}
+	
+	public boolean isPose() {
+		return pose;
+	}
+	
+	public void pose() {
+		pose=true;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getCout() {
+		return cout;
+	}
+	public void setCout(int cout) {
+		this.cout = cout;
 	}
 	public String getNom() {
 		return nom;
@@ -16,10 +43,10 @@ public class Carte {
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-	public String getCouleur() {	
-		return couleur;
+	public String gettype() {	
+		return type;
 	}
-	public void setCouleur(String couleur) {
-		this.couleur = couleur;
+	public void settype(String type) {
+		this.type = type;
 	}
 }
