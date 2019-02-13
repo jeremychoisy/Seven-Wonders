de52016c647bf68e54f4c7a446361c78bf7ef251
Kloril <maxime433.prevot@gmail.com>
2019-02-13 22:09:33 +0100

    Implémentation de la classe Jeton

--- Model/src/main/java/org/Model/Jeton.java
+++ Model/src/main/java/org/Model/Jeton.java
@@ -1,5 +1,21 @@
 package org.Model;
 
 public class Jeton {
-
+	private int valeur;
+	
+	public Jeton(){}
+	
+	public Jeton(int valeur) {
+		this.valeur=valeur;
+	}
+	
+	public int getValeur() {
+		return valeur;
+	}
+	
+	public void setValeur(int valeur) {
+		if(valeur != -1 && valeur !=1 && valeur != 3 && valeur !=5) {
+			System.out.println("Mauvaise valeur rentrée, Les valeurs disponibles pour les jetons sont -1, 1, 3, 5" );
+		}
+	}
 }
--- Model/src/main/java/org/Model/Merveille.java
+++ Model/src/main/java/org/Model/Merveille.java
@@ -4,30 +4,36 @@ public class Merveille {
 	private String nom;
 	private String ressource;
 	private int etapeCourante;
-	
-	public Merveille() {}
-	
-	public Merveille(String nom, String ressource) {
-		this.nom=nom;
-		this.ressource=ressource;
-		etapeCourante=1;
+
+	public Merveille() {
 	}
-	
+
+	public Merveille(String nom, String ressource) {
+		this.nom = nom;
+		this.ressource = ressource;
+		etapeCourante = 1;
+	}
+
 	public String getNom() {
 		return nom;
 	}
+
 	public void setNom(String nom) {
-		this.nom=nom;
+		this.nom = nom;
 	}
+
 	public String getRessource() {
 		return ressource;
 	}
+
 	public void setRessource(String ressource) {
-		this.ressource=ressource;
+		this.ressource = ressource;
 	}
+
 	public int getEtapeCourante() {
 		return etapeCourante;
 	}
+
 	public void changeEtape() {
 		etapeCourante++;
 	}
