f23150d3ae573d0b8bf0bc587a0d97f368ccae64
maku <mmillier@hotmail.fr>
2019-02-13 23:45:21 +0100

    modification de la class joueur avec la main du joueur et une liste de ressource

--- Model/src/main/java/org/Model/assets/Joueur.java
+++ Model/src/main/java/org/Model/assets/Joueur.java
@@ -1,17 +1,24 @@
 package org.Model.assets;
 
+
+import java.util.HashMap;
+
 import com.corundumstudio.socketio.SocketIOClient;
 
 public class Joueur {
-	private int id;
 	private String name;
 	private SocketIOClient socket;
+	private Main main;
+	private HashMap<String,Integer> ressource;
 	
-	public Joueur(String name, int id, SocketIOClient socket) {
+	
+	public Joueur(String name, SocketIOClient socket, Main main, HashMap<String,Integer> ressource ) {
 		this.name = name;
-		this.id = id;
 		this.socket =socket;
+		this.main = main;
+		this.setRessource(ressource);
 	}
+	
 	public String getName() {
 		return name;
 	}
@@ -19,18 +26,29 @@ public class Joueur {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
 	public SocketIOClient getSocket() {
 		return socket;
 	}
 	public void setSocket(SocketIOClient socket) {
 		this.socket = socket;
 	}
+
+	public Main getMain() {
+		return main;
+	}
+
+	public void setMain(Main main) {
+		this.main = main;
+	}
+
+	public HashMap<String,Integer> getRessource() {
+		return ressource;
+	}
+
+	public void setRessource(HashMap<String,Integer> ressource) {
+		this.ressource = ressource;
+	}
+	
 
 
 }
