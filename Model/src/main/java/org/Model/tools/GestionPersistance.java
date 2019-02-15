7bcfe0331162ce309c7fc1211da6c214ae24bec1
jeremychoisy <choisy.jeremy@gmail.com>
2019-02-15 10:44:30 +0100

    Update Persistance

--- Model/src/main/java/org/Model/tools/GestionPersistance.java
+++ Model/src/main/java/org/Model/tools/GestionPersistance.java
@@ -22,21 +22,43 @@ public class GestionPersistance {
 		FileWriter writer = null;
 		try {
 			writer = new FileWriter("/home/jeremy/eclipse-workspace/seven-wonders/Assets/cartes.json");
-			Carte[] c = new Carte[14];
-			c[0] = new Carte("Marché","Commerce",0);
-			c[1] = new Carte("Tour","Militaire",0);
-			c[2] = new Carte("Champs","Agricole",2);
-			c[3] = new Carte("Echoppe","Commerce",3);
-			c[4] = new Carte("Caserne","Militaire",2);
-			c[5] = new Carte("Boucher","Commerce",2);
-			c[6] = new Carte("Boulangerie","Commerce",0);
-			c[7] = new Carte("Poissonier","Commerce",0);
-			c[8] = new Carte("Etable","Militaire",3);
-			c[9] = new Carte("Ferme","Agricole",2);
-			c[10] = new Carte("Tisseur","Commerce",2);
-			c[11] = new Carte("Flotte","Militaire",2);
-			c[12] = new Carte("Cordonnier","Commerce",3);
-			c[13] = new Carte("Armurerie","Commerce",2);
+			Carte[] c = new Carte[15];
+			c[0] = new Carte("Prêteur sur Gage","Bâtiment Civil",3);
+			c[1] = new Carte("Bains","Bâtiment Civil",3);
+			c[2] = new Carte("Autel","Bâtiment Civil",2);
+			c[3] = new Carte("Théâtre","Bâtiment Civil",2);
+			c[4] = new Carte("Prêteur sur Gage","Bâtiment Civil",3);
+			c[5] = new Carte("Bains","Bâtiment Civil",3);
+			c[6] = new Carte("Autel","Bâtiment Civil",2);
+			c[7] = new Carte("Théâtre","Bâtiment Civil",2);
+			c[8] = new Carte("Palissade","Conflit Militaire",0);
+			c[9] = new Carte("Caserne","Conflit Militaire",0);
+			c[10] = new Carte("Tour de Garde","Conflit Militaire",0);
+			c[11] = new Carte("Taverne","Bâtiment Commercial",0);
+			c[12] = new Carte("Comptoir Est","Bâtiment Commercial",0);
+			c[13] = new Carte("Comptoir Ouest","Bâtiment Commercial",0);
+			c[14] = new Carte("Marché","Bâtiment Commercial",0);
+//			c[15] = new Carte("Officine","Bâtiment Scientifique",0);
+//			c[16] = new Carte("Atelier","Bâtiment Scientifique",0);
+//			c[17] = new Carte("Scriptorium","Bâtiment Scientifique",0);
+//            c[18] = new Carte("Métier à tisser","Produit Manufacturé",0);
+//            c[19] = new Carte("Verrerie","Produit Manufacturé",0);
+//            c[20] = new Carte("Presse","Produit Manufacturé",0);
+//            c[21] = new Carte("Métier à tisser","Produit Manufacturé",0);
+//            c[22] = new Carte("Verrerie","Produit Manufacturé",0);
+//            c[23] = new Carte("Presse","Produit Manufacturé",0);
+//            c[24] = new Carte("Guide des Travailleurs","Guilde",0);
+//            c[25] = new Carte("Guilde des Artisans","Guilde",0);
+//            c[26] = new Carte("Guilde des Commerçants","Guilde",0);
+//            c[27] = new Carte("Guilde des Philosophes","Guilde",0);
+//            c[28] = new Carte("Guilde des Espions","Guilde",0);
+//            c[29] = new Carte("Guilde des Stratèges","Guilde",0);
+//            c[30] = new Carte("Guilde des Armateurs","Guilde",0);
+//            c[31] = new Carte("Guilde des Scientifiques","Guilde",0);
+//            c[32] = new Carte("Guilde des Magistrats","Guilde",0);
+//            c[33] = new Carte("Guilde des Bâtisseurs","Guilde",0);
+
+
 			gson.toJson(c, writer);
 			writer.flush();
 			writer.close();
@@ -51,7 +73,6 @@ public class GestionPersistance {
 			writer.close();
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}			
 	}
