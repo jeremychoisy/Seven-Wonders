047a5dd36e0ea27b5c0be90833a1ddfd7a48ed24
Kloril <maxime433.prevot@gmail.com>
2019-02-15 19:20:36 +0100

    Implémentation des tests unitaires du module Model

--- Model/src/test/java/org/Model/AppTest.java
+++ Model/src/test/java/org/Model/AppTest.java
@@ -1,32 +1,30 @@
 package org.Model;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.*;
+import org.junit.jupiter.api.Test;
+
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
+    
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+   
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+  
 
     /**
      * Rigourous Test :-)
